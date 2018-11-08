34704523fc896290d0ae42d4ab37467dc4c56d52
tlian7 <tlian@stanford.edu>
2018-11-08 11:21:48 -0800

    Updated lcaExample with high resolution cropped images.

--- lcaExample/lcaExample.m
+++ lcaExample/lcaExample.m
@@ -7,83 +7,110 @@
 ieInit;
 
 %% Load the data
-% We have two sets of data, one where the letters are placed far (1.8 1.2
-% 0.6 dpt) and another one where the letters are placed closer (8.33, 4.55,
-% 3.12 dpt)
-% Not sure yet which one shows LCA better.
+% We have two sets of data, one where the letters are placed at 1.8 1.2 0.6
+% dpt.
 
+accom = [1.8 1.2 0.6];
+r_Azoom_px = [121   240    56    56];
+r_Bzoom_px = [317   293    56    56];
+r_Czoom_px = [560   335    56    56];
+        
 dirName = 'lcaExample_far'; % far data
-% dirName = 'lcaExample_close'; % close data
-datadir = ileFetchDir(dirName);
+dataDir = ileFetchDir(dirName);
 
-if(strcmp(dirName,'lcaExample_close'))
+if(strcmp(dirName,'lcaExample_far'))
     
-     % Close
-    focusA = load(fullfile(dataDir,...
-        'lettersAtDepth_8.33dpt.mat'));
-    focusB = load(fullfile(dataDir,...
-        'lettersAtDepth_4.55dpt.mat'));
-    focusC = load(fullfile(dataDir,...
-        'lettersAtDepth_3.12dpt.mat'));
-    r_Azoom_px = [129   243    70    70];
-    r_Bzoom_px = [340   310    70    70];
-    r_Czoom_px = [574   334    70    70];
-    
-elseif(strcmp(dirName,'lcaExample_far'))
-    
-    % Far
-    focusA = load(fullfile(dataDir,...
-        'lettersAtDepth_1.80dpt.mat'));
-    focusB = load(fullfile(dataDir,...
-        'lettersAtDepth_1.20dpt.mat'));
-    focusC = load(fullfile(dataDir,...
-        'lettersAtDepth_0.60dpt.mat'));
-    r_Azoom_px = [121   240    56    56];
-    r_Bzoom_px = [317   293    56    56];
-    r_Czoom_px = [560   335    56    56];
+    for ii = 1:length(accom)
+        
+        fullImages{ii} = load(fullfile(dataDir,...
+            sprintf('lettersAtDepth_%0.2fdpt.mat',accom(ii))));
+       
+        % We've rendered the zoomed in rectangles defined above (e.g.
+        % r_Azoom_px) with a higher resolution, so we'll load those directly
+        % instead of cropping the full image.
+        for jj = 1:3
+            cropImages{ii,jj} = load(fullfile(dataDir,...
+                sprintf('lettersAtDepth_%0.2fdpt_%i.mat',accom(ii),jj)));
+            % Note: the angular support on these cropped optical images is
+            % not correct. This is a bug that needs to be fixed; it wasn't
+            % set correctly during rendering.
+        end
+        
+        % Show the optical images
+        % ieAddObject(fullImages{ii});
+        % oiWindow;
+        
+    end
     
 end
 
-% Show the optical images
-% ieAddObject(focusA.oi);
-% ieAddObject(focusB.oi);
-% ieAddObject(focusC.oi);
-% oiWindow;
 
-%% Make figures with cropped views
+%% Put figure together
+% Each row is a different focus/accommodation. Each column is a different
+% zoomed in portion fo the scene.
 
-x = focusA.scene3d.angularSupport;
+% Should be the same for all images
+full_angSupport = fullImages{1}.scene3d.angularSupport;
 
-% Crop rectanges
-[r_Azoom_deg, Azoom_x, Azoom_y] = convertRectPx2Ang(r_Azoom_px,x);
-[r_Bzoom_deg, Bzoom_x, Bzoom_y] = convertRectPx2Ang(r_Bzoom_px,x);
-[r_Czoom_deg, Czoom_x, Czoom_y] = convertRectPx2Ang(r_Czoom_px,x);
+[r_Azoom_deg, Azoom_x, Azoom_y] = convertRectPx2Ang(r_Azoom_px,full_angSupport);
+[r_Bzoom_deg, Bzoom_x, Bzoom_y] = convertRectPx2Ang(r_Bzoom_px,full_angSupport);
+[r_Czoom_deg, Czoom_x, Czoom_y] = convertRectPx2Ang(r_Czoom_px,full_angSupport);
+r_zoom_inDeg = [r_Azoom_deg; r_Bzoom_deg; r_Czoom_deg];
+r_zoom_angSupport_x = [Azoom_x; Bzoom_x; Czoom_x];
+r_zoom_angSupport_y = [Azoom_y; Bzoom_y; Czoom_y];
 
-rgbFocusA = oiGet(focusA.oi,'rgb');
-rgbFocusA_CropA = imcrop(rgbFocusA,r_Azoom_px);
-rgbFocusA_CropB = imcrop(rgbFocusA,r_Bzoom_px);
-rgbFocusA_CropC = imcrop(rgbFocusA,r_Czoom_px);
+figure(1); clf;
+k = 1;
+for ii = 1:length(accom)
+    
+    fullRGB = oiGet(fullImages{ii}.oi,'rgb');
+    subplot(length(accom),4,k); k = k+1;
+    image(full_angSupport,full_angSupport,fullRGB);
+    axis image; xlabel('deg');
+    rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',2)
+    rectangle('Position',r_Bzoom_deg,'EdgeColor','g','LineWidth',2)
+    rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',2)
+    
+    rectColors = {'r','g','m'};
+    for jj = 1:3
+        
+        cropRGB = oiGet(cropImages{ii,jj}.oi,'rgb');
+        
+        subplot(length(accom),4,k); k = k+1;
+        
+        % We have to resample this because when we rendered we didn't set
+        % the angular support correctly. This is kind of hack.
+        curr_angSupport_x = r_zoom_angSupport_x(jj,:);
+        x1 = linspace(0,1,length(curr_angSupport_x));
+        x2 = linspace(0,1,size(cropRGB,2));
+        curr_angSupport_x = interp1(x1,curr_angSupport_x,x2);
+        
+        curr_angSupport_y = r_zoom_angSupport_y(jj,:);
+        y1 = linspace(0,1,length(curr_angSupport_y));
+        y2 = linspace(0,1,size(cropRGB,1));
+        curr_angSupport_y = interp1(y1,curr_angSupport_y,y2);
+        
+        image(curr_angSupport_x,curr_angSupport_y,cropRGB);
+        axis image; xlabel('deg');
+        rectangle('Position',r_zoom_inDeg(jj,:),...
+                  'EdgeColor',rectColors{jj},...
+                  'LineWidth',4)
 
-rgbFocusB = oiGet(focusB.oi,'rgb');
-rgbFocusB_CropA = imcrop(rgbFocusB,r_Azoom_px);
-rgbFocusB_CropB = imcrop(rgbFocusB,r_Bzoom_px);
-rgbFocusB_CropC = imcrop(rgbFocusB,r_Czoom_px);
+    end
+    
+end
 
-rgbFocusC = oiGet(focusC.oi,'rgb');
-rgbFocusC_CropA = imcrop(rgbFocusC,r_Azoom_px);
-rgbFocusC_CropB = imcrop(rgbFocusC,r_Bzoom_px);
-rgbFocusC_CropC = imcrop(rgbFocusC,r_Czoom_px);
-
+%{
 figure(1);
 subplot(3,4,1);
-image(x,x,rgbFocusA); 
+image(x,x,rgbFocusA);
 title('Focus on "A"');
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',2)
 rectangle('Position',r_Bzoom_deg,'EdgeColor','g','LineWidth',2)
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',2)
 subplot(3,4,2);
-image(Azoom_x,Azoom_y,rgbFocusA_CropA); 
+image(Azoom_x,Azoom_y,rgbFocusA_CropA);
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',4)
 subplot(3,4,3);
@@ -96,14 +123,14 @@ axis image; xlabel('deg');
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',4)
 
 subplot(3,4,5);
-image(x,x,rgbFocusB); 
+image(x,x,rgbFocusB);
 title('Focus on "B"');
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',2)
 rectangle('Position',r_Bzoom_deg,'EdgeColor','g','LineWidth',2)
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',2)
 subplot(3,4,6);
-image(Azoom_x,Azoom_y,rgbFocusB_CropA); 
+image(Azoom_x,Azoom_y,rgbFocusB_CropA);
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',4)
 subplot(3,4,7);
@@ -116,14 +143,14 @@ axis image; xlabel('deg');
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',4)
 
 subplot(3,4,9);
-image(x,x,rgbFocusC); 
+image(x,x,rgbFocusC);
 title('Focus on "C"');
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',2)
 rectangle('Position',r_Bzoom_deg,'EdgeColor','g','LineWidth',2)
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',2)
 subplot(3,4,10);
-image(Azoom_x,Azoom_y,rgbFocusC_CropA); 
+image(Azoom_x,Azoom_y,rgbFocusC_CropA);
 axis image; xlabel('deg');
 rectangle('Position',r_Azoom_deg,'EdgeColor','r','LineWidth',4)
 subplot(3,4,11);
@@ -134,6 +161,7 @@ subplot(3,4,12);
 image(Czoom_x,Czoom_y,rgbFocusC_CropC);
 axis image; xlabel('deg');
 rectangle('Position',r_Czoom_deg,'EdgeColor','m','LineWidth',4)
+%}
 
 % Increase font size
 set(findall(gcf,'-property','FontSize'),'FontSize',14)
