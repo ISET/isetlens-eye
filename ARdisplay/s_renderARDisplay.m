70df024f6205a248b9edcf62378f9731ec3a8274
tlian7 <tlian@stanford.edu>
2019-07-05 13:54:11 -0700

    Update to AR script

--- ARdisplay/s_renderARDisplay.m
+++ ARdisplay/s_renderARDisplay.m
@@ -27,10 +27,17 @@ saveDir = fullfile(isetlenseyeRootPath,'ARdisplay','output');
 if(~exist(saveDir,'dir'))
     mkdir(saveDir);
 end
+saveFlag = true;
 
 % Global rays/resolution 
-globalRays = 32;
-globalRes = 128;
+globalRays = 128;
+globalRes  = 256;
+
+% Global eye parameters
+accomm    = 1;
+pupilDiam = 4;
+caBand    = 8;
+fov       = 40;
 
 %% Step 1
 % Render an the background + the object through the eye. This is the
@@ -60,9 +67,10 @@ sceneWindow(scene);
 %}
 
 % Set eye parameters
-thisEye.accommodation = 1; % Front text
-thisEye.pupilDiameter = 4;
-thisEye.numCABands = 8; % Include chromatic aberration
+thisEye.accommodation = accomm; 
+thisEye.pupilDiameter = pupilDiam;
+thisEye.numCABands = caBands; 
+thisEye.fov = fov;
 
 % Rendering parameters
 thisEye.debugMode = false;
@@ -71,6 +79,7 @@ thisEye.resolution = globalRes;
 
 thisEye.name = 'oiRef';
 [oiRef,result] = thisEye.render();
+oiRef = oiSet(oiRef,'mean illuminance',100);
 oiWindow(oiRef);
 
 %% Step 2
@@ -95,6 +104,9 @@ thisEye.recipe = thisRecipeNoText;
 
 thisEye.name = 'oiBG';
 [oiBG,result] = thisEye.render();
+
+% Set a reasonable illuminance from the background
+oiBG = oiSet(oiBG,'mean illuminance',100);
 oiWindow(oiBG);
 
 %% Step 3
@@ -122,11 +134,14 @@ pinholeCamera.name = 'oiText';
 [sceneText,result] = pinholeCamera.render();
 sceneWindow(sceneText);
 
-%% Convert oiText into an RGB image to put on a display
+%% Step 4:
+% Render a flat 2D plane, at a certain focal distance, through the eye.
+% Texture the plane (properly) using the RGB image. These are the photons
+% coming from the display.
 
 % The display we use in PBRT is close to an sRGB display, so this should
 % work, theoretically. I anticipate needing to debug this in the future...
-rgbImage = oiGet(oiText,'rgb');
+rgbImage = oiGet(sceneText,'rgb');
 
 % Write it out
 imageTexturePath = fullfile(saveDir,'rgbImage.png');
@@ -148,9 +163,10 @@ thisEye = sceneEye('texturedPlane',...
                    'useDisplaySPD','true');
 
 % Set eye parameters
-thisEye.accommodation = 1; % Front text
-thisEye.pupilDiameter = 4;
-thisEye.numCABands = 8; % Include chromatic aberration
+thisEye.accommodation = accomm; 
+thisEye.pupilDiameter = pupilDiam;
+thisEye.numCABands = caBands; 
+thisEye.fov = fov;
 
 % Rendering parameters
 thisEye.debugMode = false;
@@ -160,7 +176,111 @@ thisEye.resolution = globalRes;
 % Render
 thisEye.name = 'oiDisplay';
 [oiDisplay, result] = thisEye.render();
+
+% Set a reasonable illuminance from the display
+oiDisplay = oiSet(oiDisplay,'mean illuminance',50);
 oiWindow(oiDisplay);
+
+%% Save OI
+if(saveFlag)
+    formatOut = 'mm_dd_HH_MM';
+    timestamp = datestr(now,formatOut);
+    fn = fullfile(saveDir,sprintf('Output_%s.mat',timestamp));
+    save(fn,'oiRef','oiDisplay','oiBG','oiText','rgbImage',...
+        'thisEye');
+end
+
+%% Step 5:
+% Add the photons from Step 4 and Step 2 together. This is the TEST retinal
+% irradiance.
+
+photonsDisplay = oiGet(oiDisplay,'photons');
+photonsBG = oiGet(oiBG,'photons');
+
+% Find scaling for display photons
+
+%{
+rgb = oiGet(oiARdisplay,'rgb');
+vcNewGraphWin();
+imshow(rgb);
+title(sprintf('Select point on object. Hit ENTER when finished.'));
+[X,Y] = getpts();
+X = round(X); Y = round(Y);
+%}
+X = 119; Y = 181; % For 256x256 resolution
+
+ptPhotonsDisplay = squeeze(photonsDisplay(Y,X,:));
+ptPhotonsBG = squeeze(photonsBG(Y,X,:));
+photonsRef = oiGet(oiRef,'photons');
+ptPhotonsRef = squeeze(photonsRef(Y,X,:));
+
+vcNewGraphWin();
+subplot(1,2,1);
+wave = oiGet(oiARdisplay,'wave');
+plot(wave,ptPhotonsDisplay); hold on;
+plot(wave,ptPhotonsRef);
+plot(wave,ptPhotonsBG);
+xlabel('Wave'); ylabel('Photons')
+legend('Display','Ground Truth','Background');
+
+scalingFactor = (ptPhotonsRef - ptPhotonsBG)./ptPhotonsDisplay;
+subplot(1,2,2);
+plot(wave,scalingFactor);
+title('Ratio: Ref/Display')
+
+minRatio = min(scalingFactor);
+vcNewGraphWin();
+plot(wave,ptPhotonsDisplay.*minRatio); hold on;
+plot(wave,ptPhotonsRef);
+xlabel('Wave'); ylabel('Photons')
+legend('Display (scaled)','Ground Truth');
+
+testRatio = max(scalingFactor);
+% Sum up, but with scaling
+photonsTotal = photonsDisplay.*testRatio + photonsBG;
+
+oiARdisplay = oiBG; % Make a copy
+oiARdisplay = oiSet(oiARdisplay,'photons',photonsTotal); % Set photons
+oiARdisplay = oiSet(oiARdisplay,'name','oiARdisplay');
+
+oiWindow(oiARdisplay);
+
+%% Plot background vs text photons
+% Debugging
+% Background is actually lighter than the object (text). There no way an
+% addition of photons will result in something similar in luminance to the
+% reference.
+
+rgb = oiGet(oiRef,'rgb');
+vcNewGraphWin();
+imshow(rgb.^(0.5)); 
+% [X, Y] = getpts();
+textPt = [118 167]; % for 256 res
+sofaPt = [122 168]; % for 256 res
+
+% Check color
+textRGB = rgb(textPt(2),textPt(1),:).^(0.5);
+sofaRGB = rgb(sofaPt(2),sofaPt(1),:).^(0.5);
+textRGB = repmat(textRGB,[100 100 1]);
+sofaRGB = repmat(sofaRGB,[100 100 1]);
+vcNewGraphWin();
+subplot(1,2,1);
+imshow(textRGB);
+subplot(1,2,2);
+imshow(sofaRGB);
+
+% Plot
+textPhotonsRef = squeeze(photonsRef(textPt(2),textPt(1),:));
+sofaPhotonsRef = squeeze(photonsRef(sofaPt(2),sofaPt(1),:));
+vcNewGraphWin();
+plot(wave,textPhotonsRef); hold on;
+plot(wave,sofaPhotonsRef);
+legend('Text (object)','Sofa (background)')
+grid on;
+xlabel('Wavelength (nm)');
+ylabel('Photons');
+
+
 
 
 
